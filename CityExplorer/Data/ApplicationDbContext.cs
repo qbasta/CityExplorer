8165f522d5a7b43b916a466fc0070450e60d5419
Jakub Feliński <80295@student.pb.edu.pl>
2023-10-13 14:47:18 +0200

    Dodaj pliki projektów.

---
+++ CityExplorer/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CityExplorer.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityExplorer.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
