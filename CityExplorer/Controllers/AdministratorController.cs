e2ae26a4d740e08d5db2e5249efc1513838f3eee
Jakub Feliński <80295@student.pb.edu.pl>
2023-11-01 19:00:40 +0100

    Admin panel page + mockup links to admin functionalities

---
+++ CityExplorer/Controllers/AdministratorController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityExplorer.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdministratorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RoleManager()
+        {
+            return RedirectToAction("Index", "RoleManager");
+        }
+
+        public IActionResult UserRoles()
+        {
+            return RedirectToAction("Index", "UserRoles");
+        }
+
+    }
+}
