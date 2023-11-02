cadf6bdffa97eb3e783170a7eea5bac476393865
Jakub Feliński <80295@student.pb.edu.pl>
2023-11-02 15:08:05 +0100

    User list display, added editing of existing users.

--- CityExplorer/Controllers/AdministratorController.cs
+++ CityExplorer/Controllers/AdministratorController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CityExplorer.Models;
+using CityExplorer.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityExplorer.Controllers
@@ -6,6 +9,15 @@ namespace CityExplorer.Controllers
     [Authorize(Roles = "Admin")]
     public class AdministratorController : Controller
     {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public AdministratorController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -19,6 +31,76 @@ namespace CityExplorer.Controllers
         public IActionResult UserRoles()
         {
             return RedirectToAction("Index", "UserRoles");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditUser(string id) 
+        {
+            
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id = {id} cannot be found";
+                return NotFound();
+            }
+            else
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+
+                var model = new EditUserViewModel
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    UserName = user.UserName,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Roles = userRoles
+
+                };
+
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUser(EditUserViewModel model)
+        {
+
+            var user = await _userManager.FindByIdAsync(model.Id);
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id = {model.Id} cannot be found";
+                return NotFound();
+            }
+            else
+            {
+                user.Email = model.Email;
+                user.UserName = model.UserName;
+                user.FirstName = model.FirstName;
+                user.LastName = model.LastName;
+
+                var result = await _userManager.UpdateAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("UsersList");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View(model); 
+
+            }
+        }
+
+
+        public IActionResult UsersList()
+        {
+            var users = _userManager.Users;
+            return View(users);
         }
 
     }
